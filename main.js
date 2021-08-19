9d69da094b6d2fcff7df96c0a287eb19f38c5a26
dakshc3109 <78908644+dakshc3109@users.noreply.github.com>
2021-08-19 19:34:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+//this is main.js
+
+Webcam.set({
+   width: 310,
+   height: 300,
+   image_format: "png",
+   png_quality: 90,
+   constrainers: {
+    facingMode: "enviroment"
+   }
+});;
+
+Webcam.attach("#camera");
+
+function take_snapshot(){
+   Webcam.snap(function(data_uri){
+      document.getElementById("result").innerHTML = "<img id='captured_img' src='"+data_uri+"'>";
+   });
+};
+
+console.log("ml5 version: ", ml5.version);
+
+var classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+
+function modelLoaded(){
+   console.log("model loaded");
+};
+
+function predict(){
+   var img = document.getElementById("captured_img");
+   classifier.classify(img, gotResult);
+};
+
+function gotResult(error, results){
+   if(error){
+      console.error();
+   }
+   else{
+      console.log(results);
+      document.getElementById("object_name").innerHTML = results[0].label;
+   }
+}
